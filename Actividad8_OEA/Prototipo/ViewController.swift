299eb9abde2cd0fae15c1734ed9be85577d12033
osvestra <81280042+osvestra@users.noreply.github.com>
2021-03-24 18:50:47 -0600

    Add files via upload

---
+++ Actividad8_OEA/Prototipo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototipo
+//
+//  Created by user182892 on 3/24/21.
+//  Copyright © 2021 user182892. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
